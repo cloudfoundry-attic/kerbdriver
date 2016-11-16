7beb222499e72c30b67042e0018dfa905a2f580f
Tommy Knowlton <tommy@w7tek.us>
2016-11-16 12:36:18 -0700

    Create and test a runas package
    * kinit as specific user to determine permissions (validate credential)
    [Create kerberized NFS driver](https://www.pivotaltracker.com/story/show/133621185)

--- runas/runas.go
+++ runas/runas.go
@@ -1,52 +1,107 @@
 package runas
 
 import (
-	"errors"
+	"fmt"
 	"math/rand"
-	"os/user"
+	osuser "os/user"
+	"strconv"
+	"syscall"
 
 	"code.cloudfoundry.org/goshims/execshim"
 	"code.cloudfoundry.org/goshims/usershim"
 	"code.cloudfoundry.org/lager"
 )
 
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-
 type usr struct {
-	user.User
+	*osuser.User
 }
 
 type User interface {
-	Run(lager.Logger, execshim.Cmd) (int, error)
+	Uid() string
+	Gid() string
+	Username() string
+	Name() string
+	HomeDir() string
 }
 
-func RandomUser(logger lager.Logger, exec execshim.Exec, user usershim.User) (User, error) {
-	b := make([]rune, 8)
+func (u usr) Uid() string {
+	return u.User.Uid
+}
+
+func (u usr) Gid() string {
+	return u.User.Gid
+}
+
+func (u usr) Username() string {
+	return u.User.Username
+}
+
+func (u usr) Name() string {
+	return u.User.Name
+}
+
+func (u usr) HomeDir() string {
+	return u.User.HomeDir
+}
+
+func randomUsername(length int) string {
+	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyz")
+
+	b := make([]rune, length)
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 
-	cmd := exec.Command("useradd", string(b))
-	if _, err := cmd.CombinedOutput(); err != nil {
-		return nil, err
-	}
+	return string(b)
+}
 
-	osu, err := user.Lookup(string(b))
+func CreateRandomUser(logger lager.Logger, exec execshim.Exec, user usershim.User) (User, error) {
+	// likely to fail unless called by a process owned by a privileged user account
+	newUsername := randomUsername(8)
+	cmd := exec.Command("useradd", newUsername)
+	err := cmd.Run()
+
 	if err != nil {
 		return nil, err
 	}
 
-	var u usr
-	u.User = *osu
+	u, err := user.Lookup(newUsername)
+	if err != nil {
+		return nil, err
+	}
 
-	return &u, nil
+	var result usr
+	result.User = u
+
+	return &result, nil
 }
 
-func DeleteUser(logger lager.Logger, u User) error {
-	return errors.New("not implemented")
+func DeleteUser(logger lager.Logger, u User, exec execshim.Exec) error {
+	cmd := exec.Command("userdel", u.Username())
+
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func (u *usr) Run(logger lager.Logger, cmd execshim.Cmd) (int, error) {
-	// prefix the cmd with `chpst`
-	return 1, nil
+func CommandAsUser(logger lager.Logger, u User, exec execshim.Exec, name string, args ...string) (execshim.Cmd, error) {
+	cmd := exec.Command(name, args...)
+	sysProcAttr := cmd.SysProcAttr()
+
+	uid, err := strconv.ParseUint(u.Uid(), 10, 32)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse uid as integer: %v", err)
+	}
+
+	gid, err := strconv.ParseUint(u.Gid(), 10, 32)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse gid as integer: %v", err)
+	}
+
+	logger.Info(fmt.Sprintf("sysprocattr=%#v", sysProcAttr))
+
+	sysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}
+	return cmd, nil
 }
--- runas/runas_test.go
+++ runas/runas_test.go
@@ -2,10 +2,12 @@ package runas_test
 
 import (
 	"os/user"
+	"syscall"
 
 	"code.cloudfoundry.org/goshims/execshim/exec_fake"
 	"code.cloudfoundry.org/goshims/usershim/user_fake"
 	"code.cloudfoundry.org/lager"
+	"code.cloudfoundry.org/lager/lagertest"
 	"github.com/lds-cf/knfsdriver/runas"
 
 	. "github.com/onsi/ginkgo"
@@ -27,31 +29,78 @@ var _ = Describe("Runas", func() {
 		//testContext context.Context
 	)
 
-	Context("given a created user", func() {
+	BeforeEach(func() {
+		logger = lagertest.NewTestLogger("runas")
+
+		fakeExec = &exec_fake.FakeExec{}
+		fakeCmd = &exec_fake.FakeCmd{}
+		fakeExec.CommandReturns(fakeCmd)
+
+		fakeUser = &user_fake.FakeUser{}
+		fakeUser.LookupReturns(&user.User{Uid: "9876", Gid: "9876"}, nil)
+
+		subject, err = runas.CreateRandomUser(logger, fakeExec, fakeUser)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	Context("#CreateRandomUser", func() {
+		AfterEach(func() {
+			err = runas.DeleteUser(logger, subject, fakeExec)
+			Expect(err).NotTo(HaveOccurred())
+		})
+		It("should call fakeExec shim to create a user", func() {
+			Expect(fakeExec.CommandCallCount()).To(Equal(1))
+			name, args := fakeExec.CommandArgsForCall(0)
+
+			Expect(name).To(Equal("useradd"))
+			Expect(len(args)).To(Equal(1))
+
+			Expect(fakeCmd.RunCallCount()).To(Equal(1))
+		})
+
+	})
+
+	Context("#CommandAsUser", func() {
+		AfterEach(func() {
+			err = runas.DeleteUser(logger, subject, fakeExec)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("should be able to run a command as that user", func() {
+			fakeCmd = &exec_fake.FakeCmd{}
+			fakeExec.CommandReturns(fakeCmd)
+			fakeSpa := &syscall.SysProcAttr{}
+			fakeCmd.SysProcAttrReturns(fakeSpa)
+
+			runasCmd, err := runas.CommandAsUser(logger, subject, fakeExec, "id", "-F")
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(fakeSpa.Credential.Uid).To(Equal(uint32(9876)))
+			Expect(fakeSpa.Credential.Gid).To(Equal(uint32(9876)))
+
+			runasCmd.Run()
+			// possibly some expectations on the output of the `id` command that we just ran?
+		})
+
+	})
+
+	Context("#DeleteUser", func() {
 		BeforeEach(func() {
 			fakeExec = &exec_fake.FakeExec{}
 			fakeCmd = &exec_fake.FakeCmd{}
-			fakeUser = &user_fake.FakeUser{}
-
 			fakeExec.CommandReturns(fakeCmd)
-			fakeUser.LookupReturns(&user.User{}, nil)
+		})
+		It("should be deletable", func() {
+			err = runas.DeleteUser(logger, subject, fakeExec)
+			Expect(err).NotTo(HaveOccurred())
 
-			subject, err = runas.RandomUser(logger, fakeExec, fakeUser)
-			Expect(err).NotTo(HaveOccurred())
-		})
-		AfterEach(func() {
-			err = runas.DeleteUser(logger, subject)
-			Expect(err).NotTo(HaveOccurred())
-		})
-		FIt("should call exec with the correct args", func() {
-			Expect(fakeCmd.CombinedOutputCallCount()).To(Equal(1))
-		})
-		It("should be able to run a command as that user", func() {
-			var rc int
-			rc, err = subject.Run(logger, fakeCmd)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(rc).To(Equal(0))
-			Expect(fakeCmd.CombinedOutputCallCount()).To(Equal(1))
+			Expect(fakeExec.CommandCallCount()).To(Equal(1))
+			name, args := fakeExec.CommandArgsForCall(0)
+
+			Expect(name).To(Equal("userdel"))
+			Expect(len(args)).To(Equal(1))
+
+			Expect(fakeCmd.RunCallCount()).To(Equal(1))
 		})
 	})
 })
